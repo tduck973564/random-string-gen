a76b7806984fc0ca677467dcc7a7a3464dc54ccd
tduck973564 <thomas@localhost.localdomain>
2020-11-23 16:38:26 +1100

    put in effort

--- garbage-gen.py
+++ garbage-gen.py
@@ -1,5 +1,5 @@
-from string import ascii_letters; from random import randrange
-chars = list(ascii_letters); chars.extend(list("1234567890!@#$%^&*()-=_+[];':,."))
+from random import randrange
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '|', ';', ':', ',', '.', '<', '>', '/', '?', '`', '~']
 returnlist = []
 
 while True:
